ba81fb79e0978f6f6e63e6120804a088d958fd34
Rafael Soares <rafaelsantos88@gmail.com>
2013-11-22 02:21:09 -0200

    Implementando novo layout

---
+++ footer.php
@@ -0,0 +1,42 @@
+<!-- BEGIN CORE JS FRAMEWORK--> 
+<script src="assets/plugins/jquery-1.8.3.min.js" type="text/javascript"></script> 
+<script src="assets/plugins/jquery-ui/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script> 
+<script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script> 
+<script src="assets/plugins/breakpoints.js" type="text/javascript"></script> 
+<script src="assets/plugins/jquery-unveil/jquery.unveil.min.js" type="text/javascript"></script> 
+<!-- END CORE JS FRAMEWORK --> 
+<!--[if lt IE 9]>
+	<script src="assets/plugins/excanvas.js"></script>
+	<script src="assets/plugins/respond.js"></script>	
+	<![endif]--> 
+
+<!-- BEGIN PAGE LEVEL JS --> 
+<script src="assets/plugins/pace/pace.min.js" type="text/javascript"></script>  
+<script src="assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script> 
+<script src="assets/plugins/jquery-numberAnimate/jquery.animateNumbers.js" type="text/javascript"></script> 
+<script src="assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js" type="text/javascript"></script>  
+<script src="assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script> 
+<script src="assets/plugins/jquery-block-ui/jqueryblockui.js" type="text/javascript"></script> 
+<script src="assets/plugins/bootstrap-select2/select2.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery-ricksaw-chart/js/raphael-min.js"></script> 
+<script src="assets/plugins/jquery-ricksaw-chart/js/d3.v2.js"></script>
+<script src="assets/plugins/jquery-ricksaw-chart/js/rickshaw.min.js"></script>
+<script src="assets/plugins/jquery-morris-chart/js/morris.min.js"></script>
+<script src="assets/plugins/jquery-easy-pie-chart/js/jquery.easypiechart.min.js"></script>
+<script src="assets/plugins/jquery-slider/jquery.sidr.min.js" type="text/javascript"></script> 	
+<script src="assets/plugins/jquery-jvectormap/js/jquery-jvectormap-1.2.2.min.js" type="text/javascript"></script> 	
+<script src="assets/plugins/jquery-jvectormap/js/jquery-jvectormap-us-lcc-en.js" type="text/javascript"></script> 	
+<script src="assets/plugins/jquery-sparkline/jquery-sparkline.js"></script>
+<script src="assets/plugins/jquery-flot/jquery.flot.min.js"></script>
+<script src="assets/plugins/jquery-flot/jquery.flot.animator.min.js"></script>
+<script src="assets/plugins/skycons/skycons.js"></script>
+<!-- END PAGE LEVEL PLUGINS --> 	
+<!-- PAGE JS --> 	
+<script src="assets/js/dashboard.js" type="text/javascript"></script> 
+<!-- BEGIN CORE TEMPLATE JS --> 
+<script src="assets/js/core.js" type="text/javascript"></script> 
+<script src="assets/js/demo.js" type="text/javascript"></script> 
+<!-- END CORE TEMPLATE JS -->
+</body>
+</html>
+
--- main.php
+++ main.php
@@ -3,119 +3,31 @@
 <?php
 $home = _t('HOME_PAGE', $_SESSION['SESS_LANG']);
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>Dashboard Web</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <!-- Le styles -->
-        <link href="./css/bootstrap.css" rel="stylesheet">
-        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-        <!--[if lt IE 9]>
-            <script src="../assets/js/html5shiv.js"></script>
-        <![endif]-->
-        <!-- Fav and touch icons -->
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="./ico/apple-touch-icon-144-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="./ico/apple-touch-icon-114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="./ico/apple-touch-icon-72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="./ico/apple-touch-icon-57-precomposed.png">
-        <link rel="shortcut icon" href="./ico/favicon.png">
-        <style type="text/css">
-            #container {
-                padding-top: 60px !important;
-                width: 940px !important;
-            }
-            .container > hr {
-                margin: 60px 0;
-            }
-            /* Main marketing message and sign up button */
-            .jumbotron {
-                margin: 40px 0;
-                text-align: center;
-            }
-            .jumbotron h1 {
-                font-size: 50px;
-                line-height: 1;
-            }
-            .jumbotron .lead {
-                font-size: 24px;
-                line-height: 1.25;
-            }
-            .jumbotron .btn {
-                font-size: 21px;
-                padding: 14px 24px;
-            }
-            /* Supporting marketing content */
-            .marketing {
-                margin: 60px 0;
-            }
-            .marketing p + h4 {
-                margin-top: 20px;
-            }
-            /* Customize the navbar links to be fill the entire space of the .navbar */
-        </style>
-  		   
-		<link rel="stylesheet" type="text/css" href="./css/style.css">
-	    <link rel="stylesheet" type="text/css" href="./css/bootstrap-responsive.css">
 
-    </head>
-    
-    <body>
-        <!-- Part 1: Wrap all page content here -->
-        <div id="wrap">
-            <!-- NAVBAR -->
-			<?php include 'menu.php'; ?>
-	        <!-- /.navbar-wrapper -->
-     
-       		<div class="container">
-                <!-- Jumbotron -->
-                <div class="jumbotron">
-                     <h1>SM2015 Dashboard</h1>
+<?php include_once('./header.php') ?>
 
-                    <p class="lead"><?php echo($home); ?></p>
+<!-- BEGIN CONTAINER -->
+<div class="page-container row-fluid"> 
 
-                </div>
-                <hr>
-                <!-- Example row of columns -->
-                <div class="row-fluid">
-                    <div class="span4">
-                         <h2><?=_t('Milestone', $_SESSION['SESS_LANG']); ?></h2>
+    <?php include_once('./sidebar.php') ?>
 
-                        <p><?=_t('Milestone_Home', $_SESSION['SESS_LANG']); ?></p>
-                        <p><a class="btn" href="./milestones.php"><?=_t('Manage', $_SESSION['SESS_LANG']); ?> <?=_t('Milestone', $_SESSION['SESS_LANG']); ?> &raquo;</a></p>
-                    </div>
-                    <div class="span4">
-                         <h2><?=_t('Performance', $_SESSION['SESS_LANG']); ?></h2>
+  <!-- BEGIN PAGE CONTAINER-->
+  <div class="page-content"> 
+    <!-- BEGIN SAMPLE PORTLET CONFIGURATION MODAL FORM-->
+    <div id="portlet-config" class="modal hide">
+      <div class="modal-header">
+        <button data-dismiss="modal" class="close" type="button"></button>
+        <h3>Widget Settings</h3>
+      </div>
+      <div class="modal-body"> Widget settings form goes here </div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="content">  
 
-                        <p><?=_t('Performance_Home', $_SESSION['SESS_LANG']); ?></p>
-                        <p><a class="btn" href="./milestones.php"><?=_t('Manage', $_SESSION['SESS_LANG']); ?> <?=_t('Performance', $_SESSION['SESS_LANG']); ?> &raquo;</a></p>
-                    </div>
-                    <div class="span4">
-                         <h2><?=_t('Risks', $_SESSION['SESS_LANG']); ?></h2>
+	</div> 
+  </div>  
+  <!-- END PAGE --> 
+</div>
+<!-- END CONTAINER --> 
 
-                        <p><?=_t('Risks_Home', $_SESSION['SESS_LANG']); ?></p>
-                        <p><a class="btn" href="./milestones.php"><?=_t('Manage', $_SESSION['SESS_LANG']); ?> <?=_t('Risks', $_SESSION['SESS_LANG']); ?> &raquo;</a></p>
-                    </div>
-                </div>
-                <hr>
-            </div>
-        </div>
-        <div id="footer">
-            <div class="container">
-                <p class="muted credit">© Copyright 2012. Banco Interamericano de Desarrollo, como administrador del Fondo Mesoamericano de Salud - <a href="http://sm2015.org">Salud Mesoamérica 2015</a>.</p>
-            </div>
-        </div>
-        <!-- Le javascript==================================================- ->
-		    <!-- Placed at the end of the document so the pages load faster -->
-        <script src="http://code.jquery.com/jquery.js"></script>
-        <script src="./js/bootstrap.min.js"></script>
-        <script src="./js/utils.js"></script>
-        <script src="./js/services.js"></script>
-        <script src="./js/dashboard.js"></script>
-        <script src="./js/menu.js"></script>
-    </body>
-</html>
+<?php include_once('./footer.php') ?>
