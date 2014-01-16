a02fea38d9f1194ddc22469c11de6f76bb7abeb5
Rafael Soares <rafaelsantos88@gmail.com>
2014-01-15 22:45:03 -0200

    Fix em mapa

--- dashboard/website/views.py
+++ dashboard/website/views.py
@@ -28,25 +28,28 @@ def index(request):
     maps = Map.objects.all()
     countries_map = []
     for country_map in maps:
-        table_avances = AvanceFisicoFinanciero.objects.get(country=country_map.country)
-        variation_physical = table_avances.avance_fisico_planificado - table_avances.avance_fisico_real
-        variation_financial = table_avances.avance_financiero_planificado - table_avances.avance_financiero_actual
-        if variation_physical <= 25 and variation_financial <= 25:
-            pin_color = 'green'
-        elif variation_physical >= 25 or variation_financial >= 25:
-            pin_color = 'yellow'
-        elif variation_physical >= 25 and variation_financial >= 25:
-            pin_color = 'red'
+        try:
+            table_avances = AvanceFisicoFinanciero.objects.get(country=country_map.country)
+            variation_physical = table_avances.avance_fisico_planificado - table_avances.avance_fisico_real
+            variation_financial = table_avances.avance_financiero_planificado - table_avances.avance_financiero_actual
+            if variation_physical <= 25 and variation_financial <= 25:
+                pin_color = 'green'
+            elif variation_physical >= 25 or variation_financial >= 25:
+                pin_color = 'yellow'
+            elif variation_physical >= 25 and variation_financial >= 25:
+                pin_color = 'red'
 
-        country = {
-            'lat': str(country_map.country.latlng.split(',')[0]),
-            'lng': str(country_map.country.latlng.split(',')[1]),
-            'name': str(country_map.country.name),
-            'goal': str(country_map.goal),
-            'short_description': str(country_map.short_description),
-            'pin_color': pin_color
-        }
-        countries_map.append(country)
+            country = {
+                'lat': str(country_map.country.latlng.split(',')[0]),
+                'lng': str(country_map.country.latlng.split(',')[1]),
+                'name': str(country_map.country.name),
+                'goal': str(country_map.goal),
+                'short_description': str(country_map.short_description),
+                'pin_color': pin_color
+            }
+            countries_map.append(country)
+        except AvanceFisicoFinanciero.DoesNotExist:
+            continue
 
     context = RequestContext(request)
     context.update({'countries_map': countries_map})
