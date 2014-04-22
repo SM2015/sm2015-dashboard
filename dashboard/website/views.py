1a41ee3ec648cace7441b58297636fd02305d814
Rafael Soares <rafaelsantos88@gmail.com>
2014-04-22 13:15:46 -0300

    Fix em Avances

--- dashboard/website/views.py
+++ dashboard/website/views.py
@@ -21,7 +21,7 @@ def index(request):
     countries_map = []
     for country_map in maps:
         try:
-            table_avances = AvanceFisicoFinanciero.objects.get(country=country_map.country)
+            table_avances = AvanceFisicoFinanciero.objects.filter(country=country_map.country)[-1:]
             variation_physical = table_avances.avance_fisico_planificado - table_avances.avance_fisico_real
             variation_financial = table_avances.avance_financiero_planificado - table_avances.avance_financiero_actual
             if variation_physical <= 25 and variation_financial <= 25:
@@ -42,7 +42,7 @@ def index(request):
                                                       country_map.country.id)
             }
             countries_map.append(country)
-        except AvanceFisicoFinanciero.DoesNotExist:
+        except (AvanceFisicoFinanciero.DoesNotExist, IndexError):
             continue
 
 
