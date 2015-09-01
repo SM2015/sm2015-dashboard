6702ea3c82c6dead44a6938c7f850b15a2fbe1e1
Felipe Pavão <contact@felipepavao.com>
2015-08-31 22:03:15 -0300

    acertando listagem de hitos

--- dashboard/tables/views_render.py
+++ dashboard/tables/views_render.py
@@ -25,14 +25,13 @@ def render_hitos(request, country_slug):
     
     estados_actuais = EstadoActual.objects.all()
     options_estados_actuais = {}
-
+    option = {}
     for estado in estados_actuais:
         options_estados_actuais.update({
             "{id}".format(id=estado.id): str(estado.name)
         })
 
     for hito in hitos:
-        #import ipdb;ipdb.set_trace();
         if hito.estado_actual:
             option = {
                 'selected': str(hito.estado_actual.id)
